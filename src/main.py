77802db37004505154c979c7fe39b977b067bdb6
MrSeal1 <fidrus.bence@gmail.com>
2026-04-27 16:15:51 +0200

    updated output

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from vision import get_ingredients
 from vector_db import find_best_recipes
 
 def main():
-    image_path = '../image.jpg' 
+    image_path = '../image2.jpg' 
     
     if not os.path.exists(image_path):
         print(f"Error: the file ({image_path}) does not exist")
@@ -22,11 +22,28 @@ def main():
     
     best_matches = find_best_recipes(ingredients, top_k=2)
     
+    found_set = set(ingredients)
+    
     print("\n--- BEST RESULTS ---")
     for rank, (score, recipe) in enumerate(best_matches, 1):
         match_percentage = round(score * 100, 1)
+        
+        recipe_ingredients = [item.strip().lower() for item in recipe['ingredients']]
+        recipe_set = set(recipe_ingredients)
+        
+        missing_ingredients = recipe_set - found_set
+        available_ingredients = recipe_set.intersection(found_set)
+        
         print(f"\n#{rank} - {recipe['name']} (Match: {match_percentage}%)")
-        print(f"Ingredients: {', '.join(recipe['ingredients'])}")
+        
+        if available_ingredients:
+            print(f"Available ingredients: {', '.join(available_ingredients)}")
+        
+        if missing_ingredients:
+            print(f"Missing ingredients: {', '.join(missing_ingredients)}")
+        else:
+            print("You have all needed ingredients for this recipe!")
+            
         print(f"Instructions: {recipe['instructions']}")
 
 if __name__ == '__main__':
